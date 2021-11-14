c275777bd30247fc0ade31dd349ad82fdb52a954
Rafael <rafael.mportugal@gmail.com>
2021-11-14 12:45:23 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5a87972f9449c41e06efd4adbd665b52'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '95edb900-7426-4345-bf48-e60de7bc2c91'
+};
